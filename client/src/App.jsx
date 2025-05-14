3fe3aee9eb01ca1321890f5a7767eaf83ef1059b
PhamKien28 <134904000+PhamKien2803@users.noreply.github.com>
2025-05-14 23:47:44 +0700

    commit code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./page/HomePage";
 import CategoryPage from "./page/CategoryPage";
 import ProductPage from "./page/ProductPage";
-import Login from "./page/Login";
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/category/hand" element={<CategoryPage />} />
         <Route path="/product/resurrection-duet" element={<ProductPage />} />
-        <Route path="/login" element={<Login/>}/>
       </Routes>
     </Router>
   );
